0bbcaaa8a0c1e2b5474f22a56c9d4fae5e2558cc
root <root@localhost.localdomain>
2017-12-20 12:38:39 +0800

    脚本修改

--- compile.sh
+++ compile.sh
@@ -11,8 +11,6 @@ if [ -e $SRC_PATH/source.list ]; then
 fi
 #exit 0 
 find $SRC_PATH/com -name *.java > $SRC_PATH/source.list
-rm -rf $BIN_PATH/$ONSSERVER
-mkdir $BIN_PATH/$ONSSERVER
 
 javac -d $BIN_PATH/$ONSSERVER -classpath $JAR_PATH/mongodb-driver-3.6.0.jar:$JAR_PATH/mongodb-driver-core-3.6.0.jar:$JAR_PATH/bson-3.6.0.jar:$JAR_PATH/gson-2.7.jar:$JAR_PATH/lfasr-2.0.0.1005.jar:$JAR_PATH/mysql-connector-java-5.1.45-bin.jar @$SRC_PATH/source.list
 
@@ -20,4 +18,3 @@ javac -d $BIN_PATH/$ONSSERVER -classpath $JAR_PATH/mongodb-driver-3.6.0.jar:$JAR
 cp $SOURCE_PATH/*.properties $BIN_PATH
 mkdir -p $PROJECT_PATH/tmp/download
 mkdir -p $PROJECT_PATH/tmp/upload
-
--- run_mongo.sh
+++ run_mongo.sh
@@ -7,4 +7,4 @@ SRC_PATH=$PROJECT_PATH/src
 
 #java -classpath $BIN_PATH:$JAR_PATH/gson-2.7.jar com.iflytek.msp.lfasr.Test2
 #nohup java -classpath $BIN_PATH:$JAR_PATH/gson-2.7.jar:$JAR_PATH/lfasr-2.0.0.1005.jar:$JAR_PATH/mysql-connector-java-5.1.45-bin.jar  com.iflytek.msp.lfasr.IndexMongo &
-nohup java -classpath $BIN_PATH:$JAR_PATH/bson-3.6.0.jar:$JAR_PATH/gson-2.7.jar:$JAR_PATH/lfasr-2.0.0.1005.jar:$JAR_PATH/mysql-connector-java-5.1.45-bin.jar:$JAR_PATH/mongodb-driver-3.6.0.jar:$JAR_PATH/mongodb-driver-core-3.6.0.jar  com.iflytek.msp.lfasr.IndexMongo > /dev/null & 
+nohup java -classpath $BIN_PATH:$JAR_PATH/bson-3.6.0.jar:$JAR_PATH/gson-2.7.jar:$JAR_PATH/lfasr-2.0.0.1005.jar:$JAR_PATH/mysql-connector-java-5.1.45-bin.jar:$JAR_PATH/mongodb-driver-3.6.0.jar:$JAR_PATH/mongodb-driver-core-3.6.0.jar  com.iflytek.msp.lfasr.IndexMongo > /dev/null &
